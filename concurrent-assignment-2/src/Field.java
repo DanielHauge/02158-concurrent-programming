c41bb9ef9db4168ee4b8d30576063a61c8ec29fc
Daniel F. Hauge <Animcuil@gmail.com>
2021-10-04 11:21:41 +0200

    Problem 2

--- concurrent-assignment-2/src/Field.java
+++ concurrent-assignment-2/src/Field.java
@@ -6,18 +6,27 @@
 
 public class Field {
 
-    public Field() {
+    public static final int MAX_ROWS = 11;
+    public static final int MAX_COLUMNS = 12;
 
+    private Semaphore[][] Locks;
+    public Field() {
+        Locks = new Semaphore[MAX_ROWS][MAX_COLUMNS]; // TODO: Constanter for max og min pos
+        for (int i = 0; i < MAX_ROWS; i++) {
+            for (int j = 0; j < MAX_COLUMNS; j++) {
+                Locks[i][j] = new Semaphore(1);
+            }
+        }
     }
 
     /* Block until car no. may safely enter tile at pos */
     public void enter(int no, Pos pos) throws InterruptedException {
-
+        Locks[pos.col][pos.row].P();
     }
 
     /* Release tile at position pos */
     public void leave(Pos pos) {
-
+        Locks[pos.col][pos.row].V();
     }
 
 }
